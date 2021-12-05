a2f8873cd2d97e43fe057a5b37ac47bf510ffa1f
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-12-05 15:57:19 +0100

    install bootstrap-vue  e riconfigurato header con esso

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,37 @@
 <template>
   <header>
-    <div>
-        BoolFlix
-    </div>
-    <div>
+      <!-- Utilizzato BootstrapVue per l'header e la navbar solo per un primo approccio -->
+        <b-navbar toggleable="lg" fixed type="dark" variant="dark">
+            <div>
+                <b-navbar-brand tag="h1" class="mb-0" href="#">BoolFlix</b-navbar-brand>
+            </div>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-nav-form>
+                <b-form-input size="sm" class="mr-sm-2" placeholder="Search"
+                    :class="[active? 'active': '']" @keyup.enter="getSearch" v-model="inputText"
+                    id="search"
+                >
+                </b-form-input>
+                        
+                <font-awesome-icon @focus="startSearch" @click="getSearch" icon="search" 
+                    id="icon-search"
+                />
+
+                </b-nav-form>
+                
+            </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    <!-- <div>
         <input type="text" :class="[active? 'active': '']" @keyup.enter="getSearch" v-model="inputText" id="search" placeholder="Start here your search">
-        <!-- <button @click="getSearch"><font-awesome-icon icon="search" /></button> -->
         <font-awesome-icon @focus="startSearch" @click="getSearch" icon="search" />
-    </div>
+    </div> -->
 
   </header>
 </template>
@@ -45,42 +69,81 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-header{
-    height: 100px;
-    color: #fff;
-    background-color: #000;
-    display: flex;
+.navbar-expand-lg {
     justify-content: space-between;
-    align-items: center;
     padding: 0 2rem;
 
-    div:first-child{
-        color: #e50914;
-        font-size: 2rem;
-        font-weight: 900;
-        letter-spacing: .1rem;
-    }
-
-    div:nth-child(2){
+    form{
         display: flex;
-        align-items: center;
+
+        #icon-search{
+            color: #fff;
+            font-size: 1.9rem;
+            padding: 3px;
+            margin-left: 5px;
+        }
 
         #search{
             display: none;
-            padding: 6px;
-            border-radius: 10px;
-            margin-right: 10px;
+            &.active{
+                    display: block;
+                }
         }
-        #search.active{
-            display: block;
-        }
-    }
 
-    div:nth-child(2):hover{
-        input{
-            display: block;
-        }
     }
 }
+.navbar-dark .navbar-brand {
+    color: #e50914;
+    font-size: 2rem;
+    font-weight: 700;
+    letter-spacing: .2rem;
+}
+.navbar-dark .navbar-brand:hover, .navbar-dark .navbar-brand:focus {
+    color: #e50914;
+}
+.navbar{
+    min-height: 100px;
+}
+
+.navbar-collapse {
+     flex-grow: 0;
+}
+// header{
+//     height: 100px;
+//     color: #fff;
+//     background-color: #000;
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     padding: 0 2rem;
+
+//     div:first-child{
+//         color: #e50914;
+//         font-size: 2rem;
+//         font-weight: 900;
+//         letter-spacing: .1rem;
+//     }
+
+//     div:nth-child(2){
+//         display: flex;
+//         align-items: center;
+
+//         #search{
+//             display: none;
+//             padding: 6px;
+//             border-radius: 10px;
+//             margin-right: 10px;
+//         }
+//         #search.active{
+//             display: block;
+//         }
+//     }
+
+//     div:nth-child(2):hover{
+//         input{
+//             display: block;
+//         }
+//     }
+// }
 
 </style>
